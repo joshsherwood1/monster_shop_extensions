90627c5d57d064859ed3fb955e5026858452454f
Alec Wells <alec.wells47@gmail.com>
2019-09-05 16:12:28 -0600

    Refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,9 @@ class SessionsController < ApplicationController
 
     def create
       # user = User.find_by(name: params[:name])
-<<<<<<< HEAD
       # if user && user.authenticate(params[:password])
       #   session[:user_id] = user.id
       #   flash[:success] = "Login in successful!"
-=======
-      # flash[:success] = "Login in successful!"
-      # if user && user.authenticate(params[:password])
-      #   session[:user_id] = user.id
->>>>>>> f8fa27736c58ca5fa3e6e43164fde5f8ef74d3b3
-      #   redirect_to '/'
       # else
       #   render 'users/login'
       # end
