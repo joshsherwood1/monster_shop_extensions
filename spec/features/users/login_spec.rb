36c4d8315ff1227005ffab48461a08df711390f5
Alec Wells <alec.wells47@gmail.com>
2019-09-05 19:40:59 -0600

    Finish user story 13
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe "When visitor goes to login page" do
-  it "they can login and are directed to correct page" do
-    user_1 = User.create(  name: "alec",
+  it "regular users can login and are directed to correct page" do
+    user = User.create(  name: "alec",
       address: "234 Main",
       city: "Denver",
       state: "CO",
@@ -19,14 +19,47 @@ describe "When visitor goes to login page" do
     click_button "Log In"
     expect(current_path).to eq("/profile")
     expect(page).to have_content("Login in successful!")
+  end
 
+  it "merchants can login and are directed to correct page" do
+    user_1 = User.create(  name: "alec",
+      address: "234 Main",
+      city: "Denver",
+      state: "CO",
+      zip: 80204,
+      email: "alec@gmail.com",
+      password: "password",
+      role: 1
+    )
+
+    visit '/login'
+
+    fill_in :email, with: user_1.email
+    fill_in :password, with: user_1.password
+
+    click_button "Log In"
+    expect(current_path).to eq("/merchant")
+    expect(page).to have_content("Login in successful!")
+  end
+
+  it "merchants can login and are directed to correct page" do
+    user_2 = User.create(  name: "alec",
+      address: "234 Main",
+      city: "Denver",
+      state: "CO",
+      zip: 80204,
+      email: "alec@gmail.com",
+      password: "password",
+      role: 3
+    )
+
+    visit '/login'
+
+    fill_in :email, with: user_2.email
+    fill_in :password, with: user_2.password
+
+    click_button "Log In"
+    expect(current_path).to eq("/admin")
+    expect(page).to have_content("Login in successful!")
   end
 end
-# As a visitor
-# When I visit the login path
-# I see a field to enter my email address and password
-# When I submit valid information
-# If I am a regular user, I am redirected to my profile page
-# If I am a merchant user, I am redirected to my merchant dashboard page
-# If I am an admin user, I am redirected to my admin dashboard page
-# And I see a flash message that I am logged in
