a59dd490cc260e8c046cc109439c1278a5c37aeb
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-06 12:42:53 -0600

    Change users logout feature spec test so that logging in process is not stubbed

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
 describe "When a user logs out" do
   it "they are redirected to the home page, see a flash message indicating they are logged out, and their shopping cart has been cleared" do
-    user = User.create(name: 'Christopher', address: '123 Oak Ave', city: 'Denver', state: 'CO', zip: 80021, email: 'christopher@email.com', password: 'p@ssw0rd', role: 0)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/items'
+
+    within 'nav' do
+      click_link 'Register'
+    end
+
+    expect(current_path).to eq("/register")
+
+    name = "alec"
+    address = "234 Main"
+    city = "Denver"
+    state = "CO"
+    zip = 80204
+    email = "alec@gmail.com"
+    password = "password"
+    password_confirmation = "password"
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :zip, with: zip
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+
+    click_button "Submit"
+
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Welcome, #{name}")
+
     mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
     paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
     visit "/items/#{paper.id}"
@@ -11,7 +40,7 @@ describe "When a user logs out" do
     within 'nav' do
       expect(page).to have_content("Cart: 1")
     end
-    expect(page).to have_content("Logged in as #{user.name}")
+    expect(page).to have_content("Logged in as #{name}")
     click_link("Logout")
     expect(current_path).to eq("/")
     expect(page).to have_content("You have been logged out.")
@@ -24,7 +53,7 @@ describe "When a user logs out" do
     expect(page).to have_link("Register")
     expect(page).to_not have_link("Profile")
     expect(page).to_not have_link("Logout")
-    expect(page).to_not have_content("Logged in as #{user.name}")
+    expect(page).to_not have_content("Logged in as #{name}")
     within 'nav' do
       expect(page).to have_content("Cart: 0")
     end
