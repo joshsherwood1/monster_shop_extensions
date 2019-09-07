69313d7d402d51d9fbcf9a869c97eb6b27520dd4
Alec Wells <alec.wells47@gmail.com>
2019-09-06 20:32:24 -0600

    Fiz creation spec
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -1,20 +1,29 @@
-# When I fill out all information on the new order page
-# And click on 'Create Order'
-# An order is created and saved in the database
-# And I am redirected to that order's show page with the following information:
-#
-# - Details of the order:
+require 'rails_helper'
 
-# - the date when the order was created
-RSpec.describe("Order Creation") do
+describe("Order Creation") do
   describe "When I check out from my cart" do
     before(:each) do
+      @user = User.create!(  name: "alec",
+        address: "234 Main",
+        city: "Denver",
+        state: "CO",
+        zip: 80204,
+        email: "alec@gmail.com",
+        password: "password"
+      )
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
+      visit '/login'
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_button "Log In"
+      
       visit "/items/#{@paper.id}"
       click_on "Add To Cart"
       visit "/items/#{@paper.id}"
