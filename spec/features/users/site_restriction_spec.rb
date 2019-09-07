71b3b70152d5d8ff7e23963d1ff2ee3c84915937
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-07 14:28:38 -0600

    Add feature spec test to verify that registered user can visit cetain pages with the links on their navbar

--- spec/features/users/site_restriction_spec.rb
+++ spec/features/users/site_restriction_spec.rb
@@ -23,4 +23,33 @@ describe 'User Site Navigation' do
 
     end
   end
+
+  describe "when user attempts to navigate to '/profile', '/cart', '/items', or '/merchants/'" do
+    it 'they can do so' do
+      regular_user = User.create(  name: "alec",
+                          address: "234 Main",
+                          city: "Denver",
+                          state: "CO",
+                          zip: 80204,
+                          email: "alec@gmail.com",
+                          password: "password",
+                          role: 0)
+      allow_any_instance_of(ApplicationController)
+              .to receive(:current_user)
+              .and_return(regular_user)
+
+      visit "/merchants"
+      expect(current_path).to eq("/merchants")
+
+      visit "/profile"
+      expect(current_path).to eq("/profile")
+
+      visit "/items"
+      expect(current_path).to eq("/items")
+
+      visit "/cart"
+      expect(current_path).to eq("/cart")
+
+    end
+  end
 end
