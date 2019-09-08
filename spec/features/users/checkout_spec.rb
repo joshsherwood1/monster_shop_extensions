22bc9fea0bca5c24bcd5867543a8e38f2151bfaa
Alec Wells <alec.wells47@gmail.com>
2019-09-08 10:50:47 -0600

    Fix test typo
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- spec/features/users/checkout_spec.rb
+++ spec/features/users/checkout_spec.rb
@@ -41,7 +41,7 @@ describe "when regular user visits cart" do
 
     expect(current_path).to eq("/profile/orders")
     order = Order.last
-    #might need to add moe tests to make sure order is in system
+    #might need to add more tests to make sure order is in system
     expect(page).to have_content(order.id)
     expect(page).to have_content(order.name)
     expect(page).to have_content(order.address)
@@ -53,3 +53,15 @@ describe "when regular user visits cart" do
 
   end
 end
+
+# As a registered user
+# When I add items to my cart
+# And I visit my cart
+# I see a button or link indicating that I can check out
+# And I click the button or link to check out
+# An order is created in the system, which has a status of "pending"
+# That order is associated with my user
+# I am taken to my orders page ("/profile/orders")
+# I see a flash message telling me my order was created
+# I see my new order listed on my profile orders page
+# My cart is now empty
