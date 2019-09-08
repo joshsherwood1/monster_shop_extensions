94e2000df0cd3fc84fe5b984eacbf5cb9078eee6
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-08 12:27:53 -0600

    Add migration and update schema with merchant_id column in users table for users that work for a merchant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190904223912) do
+ActiveRecord::Schema.define(version: 20190907221820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20190904223912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.integer "merchant_id"
   end
 
   add_foreign_key "item_orders", "items"
