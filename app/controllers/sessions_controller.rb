91712181b714b25ec24137e24adcf5467e2e8700
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-09 13:47:39 -0600

    Create helper method in sessions controller that redirects to a certain page based on user type
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,7 @@ class SessionsController < ApplicationController
     def new
       if current_user
         flash[:error] = "Already logged in"
-        if current_merchant_employee? || current_merchant_admin?
-          redirect_to '/merchant'
-        elsif current_admin?
-          redirect_to '/admin'
-        else
-          redirect_to '/profile'
-        end
+        redirect_by_user_type
       else
         render '/users/login'
       end
@@ -20,13 +14,7 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         flash[:success] = "Login in successful!"
-        if current_merchant_employee? || current_merchant_admin?
-          redirect_to '/merchant'
-        elsif current_admin?
-          redirect_to '/admin'
-        else
-          redirect_to '/profile'
-        end
+        redirect_by_user_type
       else
         flash[:error] = "Login information incorrect"
         redirect_to '/login'
@@ -41,5 +29,16 @@ class SessionsController < ApplicationController
 
       flash[:success] = "You have been logged out."
       redirect_to '/'
+    end
+    private
+
+    def redirect_by_user_type
+      if current_merchant_employee? || current_merchant_admin?
+        redirect_to '/merchant'
+      elsif current_admin?
+        redirect_to '/admin'
+      else
+        redirect_to '/profile'
+      end
     end
   end
