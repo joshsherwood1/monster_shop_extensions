c8b813d6b37bb917dc5c0eb8e9b0544cdcd27d3b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-09 15:15:16 -0600

    Refactor merchant controller so that code is moved to method in merchant model

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,10 +2,6 @@ class Merchant::DashboardController < Merchant::BaseController
 
   def index
     @merchant = Merchant.find(current_user.merchant_id)
-    #@quantity_for_each_order_id = @merchant.item_orders.group(:order_id).sum(:quantity)
-    #@subtotal_for_each_order_id = @merchant.item_orders.group(:order_id).sum("quantity * item_orders.price")
-    @order_details = @merchant.item_orders.group(:order_id).select("sum(quantity) as total_quantity, sum(quantity * item_orders.price) as total_subtotal")
-    binding.pry
   end
 
 end
