540ab2ea73fbcc2476d4a8e518a855fd24a38f18
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-09 12:58:13 -0600

    Update merchant controller with way to get information about each order associated with merchant

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -4,6 +4,10 @@ class Merchant::DashboardController < ApplicationController
 
   def index
     @merchant = Merchant.find(current_user.merchant_id)
+    #@quantity_for_each_order_id = @merchant.item_orders.group(:order_id).sum(:quantity)
+    #@subtotal_for_each_order_id = @merchant.item_orders.group(:order_id).sum("quantity * item_orders.price")
+    @order_details = @merchant.item_orders.group(:order_id).select("sum(quantity) as total_quantity, sum(quantity * item_orders.price) as total_subtotal")
+    binding.pry
   end
 
   private
