4f9650c4318a9be666e73500aa952a77d84b691d
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-09 13:43:46 -0600

    Update schema to remove duplicate merchant_id column from users table
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190909142719) do
+ActiveRecord::Schema.define(version: 20190909190445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
