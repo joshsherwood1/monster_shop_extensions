ffe3fc8a2fe3a4ecbc8700292c4a073c2a934fe1
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-09 23:12:57 -0600

    Refactor merchant itemsorders controller so that it uses two helper methods to dry up code

--- app/controllers/merchant/itemorders_controller.rb
+++ app/controllers/merchant/itemorders_controller.rb
@@ -1,11 +1,9 @@
 class Merchant::ItemordersController < Merchant::BaseController
   def fulfill
     @item_order = ItemOrder.find(params[:id])
-    @item_order.status = "fulfilled"
-    @item_order.save
+    @item_order.fulfill_and_save_item_order
     item = Item.find(@item_order.item_id)
-    item.inventory -= @item_order.quantity
-    item.save
+    item.subtract(@item_order.quantity)
     redirect_to "/orders/#{@item_order.order_id}"
     flash[:success] = "#{Item.find(@item_order.item_id).name} is now fulfilled"
   end
