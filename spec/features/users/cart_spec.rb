80523658789a8bb55ca1e0997f14c894148bcdca
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-10 00:19:06 -0600

    Add test in users cart feature spec test that verifies that when user attempts  to put inactive item in cart that they receive flash message indicating that they
     cannot do so

--- spec/features/users/cart_spec.rb
+++ spec/features/users/cart_spec.rb
@@ -6,6 +6,7 @@ describe 'User Cart Checkout' do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @green_pencil = @mike.items.create(name: "Green Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100, active?:false)
       @user = User.create(name: 'Christopher', address: '123 Oak Ave', city: 'Denver', state: 'CO', zip: 80021, email: 'christopher@email.com', password: 'p@ssw0rd', role: 0)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
@@ -24,6 +25,15 @@ describe 'User Cart Checkout' do
       visit "/cart"
       expect(page).to_not have_content("You must login or register in order to checkout")
       expect(page).to have_link("Checkout")
+    end
+
+    it 'they will see a message saying that they cannot add inactive item to cart if they attempt to do so' do
+
+      visit "/items/#{@green_pencil.id}"
+      click_on "Add To Cart"
+
+
+      expect(page).to have_content("You cannot add #{@green_pencil.name} to your cart because it is inactive")
     end
   end
 end
