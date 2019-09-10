d464f14b81cc45de8996c18563baba9ec16b2060
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-10 00:17:06 -0600

    Remove code from merchant itemscontroller into order model in order to dry up code

--- app/controllers/merchant/itemorders_controller.rb
+++ app/controllers/merchant/itemorders_controller.rb
@@ -4,6 +4,10 @@ class Merchant::ItemordersController < Merchant::BaseController
     @item_order.fulfill_and_save_item_order
     item = Item.find(@item_order.item_id)
     item.subtract(@item_order.quantity)
+    order = Order.find(@item_order.order_id)
+    if order.item_orders.pluck(:status).all? { |status| status == "fulfilled"}
+      order.packaged
+    end
     redirect_to "/orders/#{@item_order.order_id}"
     flash[:success] = "#{Item.find(@item_order.item_id).name} is now fulfilled"
   end
