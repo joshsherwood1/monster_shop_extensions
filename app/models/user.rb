8921639b1b9e84468208266a60a20e50c64edc85
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-13 22:38:06 -0600

    Add migrations to create address table in database and relationship between user and addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,12 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :orders
+  has_many :addresses
   belongs_to :merchant, optional: true
 
   validates :name, presence: true
-  validates :address, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password_digest, require: true
-  validates_length_of :zip, :is => 5
-  validates_numericality_of :zip
 
   enum role: %w(regular_user merchant_employee merchant_admin admin )
 
