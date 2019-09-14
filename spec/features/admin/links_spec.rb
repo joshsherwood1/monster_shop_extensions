2e8ca5c57a018c334b8d23d903f7fb5ccdcfe625
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-14 14:49:26 -0600

    Update feature spec tests for admin so that they reflect changes made to schema

--- spec/features/admin/links_spec.rb
+++ spec/features/admin/links_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "As an Admin User" do
   before :each do
-    @admin = User.create(name: 'Christopher', address: '123 Oak Ave', city: 'Denver', state: 'CO', zip: 80021, email: 'christopher@email.com', password: 'p@ssw0rd', role: 3)
+    @admin = User.create(name: 'Christopher', email: 'christopher@email.com', password: 'p@ssw0rd', role: 3)
+    @admin.addresses.create!(address: '123 Oak Ave', city: 'Denver', state: 'CO', zip: 80021)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
   end
 
--- spec/features/admin/merchant_spec.rb
+++ spec/features/admin/merchant_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "Admin user can see all merchants" do
   before :each do
-    @regular_user =  User.create!(  name: "alec", address: "234 Main", city: "Denver", state: "CO", zip: 80204, email: "5@gmail.com", password: "password")
+    @regular_user =  User.create!(  name: "alec", email: "5@gmail.com", password: "password")
+    @address_1 = @regular_user.addresses.create(address: "234 Main", city: "Denver", state: "CO", zip: 80204)
     @dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210, enabled?: true)
     @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203, enabled?: false)
     @pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
@@ -10,16 +11,16 @@ describe "Admin user can see all merchants" do
     @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
     @paper = @bike_shop.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
     @pencil = @bike_shop.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-    @order_1 = @regular_user.orders.create(name: "Sam Jackson", address: "234 Main St", city: "Seattle", state: "Washington", zip: 99987, status: 0)
-    @order_2 = @regular_user.orders.create(name: "Sam Jackson", address: "234 Main St", city: "Seattle", state: "Washington", zip: 99987, status: 0)
+    @order_1 = @regular_user.orders.create(address_id: @address_1.id, status: 0)
+    @order_2 = @regular_user.orders.create(address_id: @address_1.id, status: 0)
     @itemorder = ItemOrder.create(order_id: @order_1.id, item_id: @tire.id, quantity: 2, price: 100)
     ItemOrder.create(order_id: @order_1.id, item_id: @paper.id, quantity: 2, price: 20)
     ItemOrder.create(order_id: @order_1.id, item_id: @pencil.id, quantity: 3, price: 2)
     ItemOrder.create(order_id: @order_2.id, item_id: @tire.id, quantity: 1, price: 100)
     ItemOrder.create(order_id: @order_2.id, item_id: @pencil.id, quantity: 4, price: 2)
 
-    @admin = User.create(name: 'Christopher', address: '123 Oak Ave', city: 'Denver', state: 'CO', zip: 80021, email: 'christopher@email.com', password: 'p@ssw0rd', role: 3)
-
+    @admin = User.create(name: 'Christopher', email: 'christopher@email.com', password: 'p@ssw0rd', role: 3)
+    @address_2 = @admin.addresses.create(address: "234 Main", city: "Denver", state: "CO", zip: 80204)
     visit '/login'
 
     fill_in :email, with: @admin.email
