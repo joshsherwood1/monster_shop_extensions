fc8724bf5a49d3643d7f33f38aa1097003990d7e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-14 18:27:39 -0600

    Add address instance method to address model spec and address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,4 +7,8 @@ class Address < ApplicationRecord
   validates_length_of :zip, :is => 5
   validates_numericality_of :zip
   validates :address_type, presence: true
+
+  def shipped_orders_with_address?(id)
+    self.user.orders.where(address_id: id).where(status: 2).any?
+  end
 end
